59a4278d325725ba6160e700113a47e3da5e373b
Gareth Field <field.gareth@gmail.com>
2019-07-08 18:23:43 -0400

    beginning dashboard

--- src/DashboardSite/index.js
+++ src/DashboardSite/index.js
@@ -1,7 +1,18 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  position: relative;
+  top: 0;
+  height: 100vh;
+`;
 
 export default class DashboardSite extends Component {
   render() {
-    return <div>Dashboard site!</div>;
+    return (
+      <Wrapper>
+        <div>Dashboard site!</div>
+      </Wrapper>
+    );
   }
 }
